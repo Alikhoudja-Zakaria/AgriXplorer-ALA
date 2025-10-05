5d79296e09fefd7cee634a0cd48e45278f46d97b
Alikhoudja Zakaria <alikhoudjazakariia@gmail.com>
2025-10-05 21:13:36 +0000

    try <iframe frameborder="0" src="https://itch.io/embed/3936170" width="5

--- src/components/godot-game.tsx
+++ src/components/godot-game.tsx
@@ -1,15 +1,9 @@
-
 'use client';
 
 export function GodotGame() {
   return (
-    <div className="w-full aspect-[16/9] rounded-lg overflow-hidden shadow-lg border border-border bg-card">
-      <iframe
-        src="/nasa_space_app.html"
-        className="w-full h-full border-0"
-        title="AgriXplorer Godot Game"
-        allow="cross-origin-isolated"
-      ></iframe>
+    <div className="w-full flex justify-center">
+        <iframe frameBorder="0" src="https://itch.io/embed/3936170?bg_color=1c2518&amp;fg_color=e0e0e0&amp;link_color=83c363&amp;border_color=3a5c2a" width="990" height="600"><a href="https://the-true-unknown-coder.itch.io/agrixplorers">AGRIXPLORERS by The True Unknown Coder</a></iframe>
     </div>
   );
 }
